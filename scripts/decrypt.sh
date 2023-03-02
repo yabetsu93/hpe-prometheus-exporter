e151103bd4a7fd2c7a0e67ed7a9988c79fa9bf2b
yabetsu93 <jabpau93@gmail.com>
2023-03-02 21:03:15 +0900

    improvements

--- scripts/decrypt.sh
+++ scripts/decrypt.sh
@@ -4,6 +4,6 @@ for FILE in `find . -name '*.enc.*'`
 do 
     if [ -f "$FILE" ];
     then
-        sops -d "$FILE" > "$(echo $FILE | cut -d . -f -2).dec.yaml"
+        sops -d "$FILE" > "$(echo $FILE | cut -d . -f -2).dec.env"
     fi
 done
--- scripts/encrypt.sh
+++ scripts/encrypt.sh
@@ -4,6 +4,6 @@ for FILE in `find . -name '*.dec.*'`
 do 
     if [ -f "$FILE" ];
     then
-        sops -e "$FILE" > "$(echo $FILE | cut -d . -f -2).enc.yaml"
+        sops -e "$FILE" > "$(echo $FILE | cut -d . -f -2).enc.env"
     fi
 done
